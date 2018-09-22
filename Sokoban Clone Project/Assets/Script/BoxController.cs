0c5d5d4992c4073c62c56aedf9c60ad6ae86cd7a
Yasir Haq <yasir.haq98@gmail.com>
2018-09-22 14:45:47 +0700

    Script

---
+++ Sokoban Clone Project/Assets/Script/BoxController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour {
+
+    float moveBox = 1f;
+   private Rigidbody2D boxRigidbody2d;
+    private void Start()
+    {
+        boxRigidbody2d = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        boxRigidbody2d.velocity = new Vector3(moveBox, moveBox);
+    }
+}
