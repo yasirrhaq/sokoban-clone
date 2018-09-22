3bc8dd41b8d1b22363aaf5d248721d45bae98d6a
Yasir Haq <yasir.haq98@gmail.com>
2018-09-22 14:31:11 +0700

    Sokoban Clone

---
+++ Sokoban Clone Project/Assets/Script/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed;
+    private Rigidbody2D myRigidbody2D;
+	// Use this for initialization
+	void Start () {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        myRigidbody2D.velocity = new Vector3(horizontal*movementSpeed, vertical*movementSpeed);
+	}
+}
