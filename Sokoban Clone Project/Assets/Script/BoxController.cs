8060ee4baa84c416ea2bf89dcbf43d659a0ad78e
Yasir Haq <yasir.haq98@gmail.com>
2018-09-24 14:55:14 +0700

    Add Prefabs

--- Sokoban Clone Project/Assets/Script/BoxController.cs
+++ Sokoban Clone Project/Assets/Script/BoxController.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxController : MonoBehaviour {
-
-    float moveBox = 1f;
-   private Rigidbody2D boxRigidbody2d;
+public class BoxController : MonoBehaviour
+{
     private void Start()
     {
-        boxRigidbody2d = GetComponent<Rigidbody2D>();
+        
     }
-    // Use this for initialization
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void Update()
     {
-        boxRigidbody2d.velocity = new Vector3(moveBox, moveBox);
+        
     }
 }
--- Sokoban Clone Project/Assets/Script/PlayerController.cs
+++ Sokoban Clone Project/Assets/Script/PlayerController.cs
@@ -13,9 +13,13 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        movement();
+	}
+
+    void movement() {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        myRigidbody2D.velocity = new Vector3(horizontal*movementSpeed, vertical*movementSpeed);
-	}
+        myRigidbody2D.velocity = new Vector3(horizontal * movementSpeed, vertical * movementSpeed);
+    }
 }
