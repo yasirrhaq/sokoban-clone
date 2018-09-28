2f8194b196b53990352cd7b789a6b03e04b2d1ae
Yasir Haq <yasir.haq98@gmail.com>
2018-09-28 21:15:23 +0700

    Add Win Condition

---
+++ Sokoban Clone Project/Assets/Script/WinCondition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour {
+
+    // Use this for initialization
+    private int Goals;
+	void Start () {
+        Goals = GameObject.FindGameObjectsWithTag("Goal").Length;
+        Debug.Log("test " + Goals);
+	}
+	
+	// Update is called once per frame
+    //masih kasaran
+	void Update () {
+        checkWin();
+	}
+    public void checkWin()
+    {
+        int currentGoals = 0;
+        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
+        foreach (GameObject box in boxes)
+        {
+            if (box.GetComponent<BoxController>().onGoal)
+            {
+                currentGoals += 1;
+                Debug.Log(currentGoals + "horee");
+            }
+        }
+        if (currentGoals == Goals )
+        {
+            Debug.Log("YOU WIN!");
+        }
+    }
+}
