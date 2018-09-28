7155aebd03ef0576ae57642df3a2156fbfdf1a14
Yasir Haq <yasir.haq98@gmail.com>
2018-09-28 20:49:27 +0700

    Sokoban Clone

--- Sokoban Clone Project/Assets/Script/BoxController.cs
+++ Sokoban Clone Project/Assets/Script/BoxController.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class BoxController : MonoBehaviour
 {
+    public bool onGoal;
+    private int count;
     private void Start()
     {
-        
+        count = GameObject.FindGameObjectsWithTag("Box").Length;
+        Debug.Log("ini awal "+count);
     }
 
     private void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="Goal")
+        {
+            onGoal = true;
+            count--;
+            Debug.Log("bener");
+            Debug.Log("sisa box : " + count);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onGoal = false;
+        count++;
+        Debug.Log("nambah deh : " + count);
     }
 }
---
+++ Sokoban Clone Project/Assets/Script/GoalTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour {
+
+    public Sprite sprite1;
+    public Sprite sprite2;
+    private SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //if (spriteRenderer.sprite == null)
+        //{
+        //    spriteRenderer.sprite = sprite1;
+        //}
+    }
+    void changeSprite()
+    {
+        if (spriteRenderer.sprite == sprite1)
+        {
+            spriteRenderer.sprite = sprite2;
+        }
+        else
+        {
+            spriteRenderer.sprite = sprite1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        changeSprite();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        changeSprite();
+    }
+}
