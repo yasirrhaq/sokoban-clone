9d376b0b6c4c6dbda4c21d2de375f10ed8a56b33
Yasir Haq <yasir.haq98@gmail.com>
2018-09-30 13:27:49 +0700

    Add Level 2

--- Sokoban Clone Project/Assets/Script/BoxController.cs
+++ Sokoban Clone Project/Assets/Script/BoxController.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class BoxController : MonoBehaviour
 {
     public bool onGoal;
+    public float moveTime = 0.5f;
+    private BoxCollider2D myBoxCollider2D;
+    private Rigidbody2D myRigidbody2D;
+    private void Start()
+    {
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void moveBox()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,9 +31,9 @@ public class BoxController : MonoBehaviour
             Debug.Log("truee");
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
+    /*private void OnTriggerExit2D(Collider2D collision)
     {
       onGoal = false;
         Debug.Log("falseee");
-    }
+    }*/
 }
