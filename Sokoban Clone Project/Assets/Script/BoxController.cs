44843864ec68274d6e325f94b5291037bbdc3bde
Yasir Haq <yasir.haq98@gmail.com>
2018-09-30 16:34:37 +0700

    Add something

--- Sokoban Clone Project/Assets/Script/BoxController.cs
+++ Sokoban Clone Project/Assets/Script/BoxController.cs
@@ -5,22 +5,12 @@ using UnityEngine;
 public class BoxController : MonoBehaviour
 {
     public bool onGoal;
-    public float moveTime = 0.5f;
     private BoxCollider2D myBoxCollider2D;
     private Rigidbody2D myRigidbody2D;
     private void Start()
     {
         myBoxCollider2D = GetComponent<BoxCollider2D>();
         myRigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    private void moveBox()
-    {
-
-    }
-    private void FixedUpdate()
-    {
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
