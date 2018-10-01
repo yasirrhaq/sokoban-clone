0cdb9808198937faa3b6c3ad34c1e0b38c8eebef
Yasir Haq <yasir.haq98@gmail.com>
2018-10-01 08:34:25 +0700

    Fix Some Error

--- Sokoban Clone Project/Assets/Script/BoxController.cs
+++ Sokoban Clone Project/Assets/Script/BoxController.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class BoxController : MonoBehaviour
 {
     public bool onGoal;
-    private BoxCollider2D myBoxCollider2D;
-    private Rigidbody2D myRigidbody2D;
-    private void Start()
-    {
-        myBoxCollider2D = GetComponent<BoxCollider2D>();
-        myRigidbody2D = GetComponent<Rigidbody2D>();
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
